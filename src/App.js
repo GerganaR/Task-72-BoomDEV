5728be87feedfbde2a6fa188e74185f14230d4d3
unknown <www.g.roshleva7@gmail.com>
2022-01-18 12:02:59 +0200

    updated files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import Tags from "./Tags";
 
 function App() {
+  const tags = ["boomdotdev", "task", "tags", "react"];
   return (
     <div className="App">
       <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
+        <div class="hero-body"></div>
       </section>
+      <div className="tags">
+        {" "}
+        {tags.map((tag) => (
+          <Tags tags={tag} />
+        ))}
+      </div>
       <div class="container is-fullhd">
         <div class="notification">
           Edit the <code>./src</code> folder to add components.
---
+++ src/Tags.js
@@ -0,0 +1,8 @@
+import react from "react";
+import App from "./App";
+
+function Tags(props) {
+  return <div class="tag">#{props.tags}</div>;
+}
+
+export default Tags;
