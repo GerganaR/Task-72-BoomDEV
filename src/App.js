b939e910e250f30662771b4b774245bbd8135541
unknown <www.g.roshleva7@gmail.com>
2022-01-18 12:13:59 +0200

    Update 1.2

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <section class="hero">
         <div class="hero-body"></div>
       </section>
-      <div className="tags">
+      <div class="tags">
         {" "}
         {tags.map((tag) => (
           <Tags tags={tag} />
         ))}
-      </div>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
       </div>
     </div>
   );
--- src/Tags.js
+++ src/Tags.js
@@ -1,5 +1,6 @@
 import react from "react";
 import App from "./App";
+import "./App.css";
 
 function Tags(props) {
   return <div class="tag">#{props.tags}</div>;
